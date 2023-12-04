e6971e44890117fd5d9ea4167b1aa199abfe45a1
Salman Ali <salman.ali201518@gmail.com>
2023-12-04 18:56:16 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
+
+Route::group(['middleware' => ['verify.shopify']], function () {
+    // Index
+    Route::get('/', [App\Http\Controllers\RegisterController::class, 'Register'])->name('home');
+
+});
